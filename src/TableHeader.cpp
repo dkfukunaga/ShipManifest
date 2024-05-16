522245dad64317dccba6848fa19b5d4137816b48
dkfukunaga <dkfukunaga@gmail.com>
2024-05-16 04:51:59 -0700

    I don't know

--- src/TableHeader.cpp
+++ src/TableHeader.cpp
@@ -4,20 +4,6 @@
 #include "TableHeader.h"
 
 
-const char TableType::ship[3]           = { 'S', 'H', 'P' };    // Ship table: "SHP"
-const char TableType::ship_string [3]   = { 'S', 'S', 'T' };    // ShipString table: "SST"
-const char TableType::game_character[3] = { 'G', 'C', 'H' };    // GameCharacter table: "GCH"
-const char TableType::ship_class[3]     = { 'S', 'C', 'L' };    // ShipClass table: "SCL"
-const char TableType::ship_weapons[3]   = { 'S', 'W', 'P' };    // ShipWeapons table: "SWP"
-const char TableType::component[3]      = { 'C', 'M', 'P' };    // Components table: "CMP"
-const char TableType::reactor[3]        = { 'r', 'c', 't' };    //     Reactor Component sub-table: "rct"
-const char TableType::sublight[3]       = { 's', 'b', 'l' };    //     Sublight Component sub-table: "sbl"
-const char TableType::ftl[3]            = { 'f', 't', 'l' };    //     FTL Component sub-table:  "ftl"
-const char TableType::shield[3]         = { 's', 'h', 'd' };    //     Shield Component sub-table:  "shd"
-const char TableType::weapon[3]         = { 'W', 'E', 'P' };    // Weapon table: "WEP"
-const char TableType::beam[3]           = { 'b', 'e', 'm' };    //     Beam Weapon sub-table:  "bem"
-const char TableType::kinetic[3]        = { 'k', 'i', 'n' };    //     Kinetic Weapon sub-table:  "kin"
-const char TableType::missile[3]        = { 'm', 'i', 's' };    //     Missile Weapon sub-table:  "mis"
-const char TableType::string[3]         = { 'S', 'T', 'R' };    // String table: "STR"
+
 
 
--- src/TableHeader.h
+++ src/TableHeader.h
@@ -9,21 +9,21 @@
 // Three character codes to indicate the type of table.
 // Tables are in all caps; sub-tables are in all lower case.
 namespace TableType {
-    const char ship[3];
-    const char ship_string[3];
-    const char game_character[3];
-    const char ship_class[3];
-    const char ship_weapons[3];
-    const char component[3];
-    const char reactor[3];
-    const char sublight[3];
-    const char ftl[3];
-    const char shield[3];
-    const char weapon[3];
-    const char beam[3];
-    const char kinetic[3];
-    const char missile[3];
-    const char string[3];
+    const char ship[3]              = { 'S', 'H', 'P' };    // Ship table: "SHP"
+    const char ship_string [3]      = { 'S', 'S', 'T' };    // ShipString table: "SST"
+    const char game_character[3]    = { 'G', 'C', 'H' };    // GameCharacter table: "GCH"
+    const char ship_class[3]        = { 'S', 'C', 'L' };    // ShipClass table: "SCL"
+    const char ship_weapons[3]      = { 'S', 'W', 'P' };    // ShipWeapons table: "SWP"
+    const char component[3]         = { 'C', 'M', 'P' };    // Components table: "CMP"
+    const char reactor[3]           = { 'r', 'c', 't' };    //     Reactor Component sub-table: "rct"
+    const char sublight[3]          = { 's', 'b', 'l' };    //     Sublight Component sub-table: "sbl"
+    const char ftl[3]               = { 'f', 't', 'l' };    //     FTL Component sub-table:  "ftl"
+    const char shield[3]            = { 's', 'h', 'd' };    //     Shield Component sub-table:  "shd"
+    const char weapon[3]            = { 'W', 'E', 'P' };    // Weapon table: "WEP"
+    const char beam[3]              = { 'b', 'e', 'm' };    //     Beam Weapon sub-table:  "bem"
+    const char kinetic[3]           = { 'k', 'i', 'n' };    //     Kinetic Weapon sub-table:  "kin"
+    const char missile[3]           = { 'm', 'i', 's' };    //     Missile Weapon sub-table:  "mis"
+    const char string[3]            = { 'S', 'T', 'R' };    // String table: "STR"
 };
 
 
