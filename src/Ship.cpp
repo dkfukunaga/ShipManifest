e949e4e584a58ad84b93edfa0a387cf25f652064
dkfukunaga <dkfukunaga@gmail.com>
2024-05-16 04:09:35 -0700

    reorganizing

---
+++ src/Ship.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include "Ship.h"
+
+
+
+enum class ShipFlag {
+    union_of_earth,
+    pacifica_free_states,
+    confederation_of_cobol,
+    new_sumerian_ascendency,
+    vanaheim_alliance,
+    sanbao_republic
+};
+
