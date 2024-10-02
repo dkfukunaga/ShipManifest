fbc22782575e9ad6281131f4da225f2b7c3b11d4
dkfukunaga <dkfukunaga@gmail.com>
2024-10-01 22:06:50 -0700

    added random access serialize. added deserialize functions. renamed serializeHeader to writeHeader.

--- src/Serializable.h
+++ src/Serializable.h
@@ -12,9 +12,14 @@ public:
     virtual ~Serializable() = default;
 
     uint32_t            serialize(DataFile &file) const;
+    uint32_t            serialize(DataFile &file, int64_t pos) const;
+    void                deserialize(DataFile &file);
+    void                deserialize(DataFile &file, int64_t pos);
 private:
-    virtual uint32_t    serializeHeader(DataFile &file) const = 0;
+    virtual uint32_t    writeHeader(DataFile &file) const = 0;
     virtual uint32_t    serializeData(DataFile &file) const = 0;
+    virtual void        readHeader(DataFile &file) = 0;
+    virtual void        deserializeData(DataFile &file) = 0;
 };
 
 uint32_t Serializable::serialize(DataFile &file) const {
@@ -24,10 +29,37 @@ uint32_t Serializable::serialize(DataFile &file) const {
 
     uint32_t size = 0;
 
-    size += serializeHeader(file);
-    size += serializeData(file);
+    writeHeader(file);
+    size = serializeData(file);
 
     return size;
+}
+
+uint32_t Serializable::serialize(DataFile &file, int64_t pos) const {
+    if (!file.isOpen()) {
+        return 0;
+    }
+    file.setWritePos(pos);
+
+    return serialize(file);
+}
+
+void Serializable::deserialize(DataFile &file) {
+    if (!file.isOpen()) {
+        return;
+    }
+
+    readHeader(file);
+    deserializeData(file);
+}
+
+void Serializable::deserialize(DataFile &file, int64_t pos) {
+    if (!file.isOpen()) {
+        return;
+    }
+    file.setReadPos(pos);
+
+    deserialize(file);
 }
 
 
