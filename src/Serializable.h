43ee48c4459b11ff8a1cb89b876251730e34807a
dkfukunaga <dkfukunaga@gmail.com>
2024-10-01 20:40:55 -0700

    removed DataFile field to simplify class and make it pure virtual.

--- src/Serializable.h
+++ src/Serializable.h
@@ -9,25 +9,26 @@
 
 class Serializable {
 public:
-    uint32_t            serialize() const;
-    void                setFile(DataFile &file);
-protected:
-    DataFile&           data_file_;
+    virtual ~Serializable() = default;
+
+    uint32_t            serialize(DataFile &file) const;
 private:
-    virtual uint32_t    serializeHeader() const = 0;
-    virtual uint32_t    serializeData() const = 0;
+    virtual uint32_t    serializeHeader(DataFile &file) const = 0;
+    virtual uint32_t    serializeData(DataFile &file) const = 0;
 };
 
-uint32_t Serializable::serialize() const {
+uint32_t Serializable::serialize(DataFile &file) const {
+    if (!file.isOpen()) {
+        return 0;
+    }
+
     uint32_t size = 0;
 
-    size += serializeHeader();
-    size += serializeData();
+    size += serializeHeader(file);
+    size += serializeData(file);
 
     return size;
 }
-
-void Serializable::setFile(DataFile &file) { data_file_ = file; }
 
 
 #endif
