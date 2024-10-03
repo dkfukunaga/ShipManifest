ad4ec97d6a97500b6dc1330511433177c15c3b67
dkfukunaga <dkfukunaga@gmail.com>
2024-10-02 23:08:51 -0700

    converted classes back to structs. added functions from serializable. implemented some subsystem functions.

--- src/Subsystem.h
+++ src/Subsystem.h
@@ -3,75 +3,147 @@
 #ifndef SUBSYSTEM_H
 #define SUBSYSTEM_H
 
+#include "DataFile.h"
+#include "Headers.h"
+#include "Serializable.h"
 #include <string>
 #include <cstdint>
 
-class Subsystem {
-private:
-    std::string         name_;
-    uint8_t             tier_;
-    uint16_t            mass_;
-    uint16_t            durability_;
-    int32_t             power_;
+struct Subsystem : public Serializable {
+    std::string         name;
+    uint8_t             tier;
+    uint16_t            mass;
+    uint16_t            durability;
+    int32_t             power;
 
-public:
-    Subsystem(std::string name, uint8_t tier, uint16_t mass, 
-              uint16_t durability, int32_t new_power):
-        name_(name),
-        tier_(tier),
-        mass_(mass),
-        durability_(durability),
-        power_(new_power) { };
+    Subsystem(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+              uint16_t new_durability = 0, int32_t new_power = 0):
+        name(new_name),
+        tier(new_tier),
+        mass(new_mass),
+        durability(new_durability),
+        power(new_power) { };
+    Subsystem(const Subsystem &) = default;             // default copy constructor
+    Subsystem(Subsystem &&) noexcept = default;         // default move construtor
+    virtual ~Subsystem() = default;                     // default virtual destructor
+
+    recsize_t           getSize() const;
+protected:
+    void                writeHeader(DataFile &file, index_t index, recsize_t size, offset_t offset) const;
+    index_t             readHeader(DataFile &file);
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
+};
+
+struct Reactor : Subsystem {
+    uint16_t            fuel_use;
+
+    Reactor(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+            uint16_t new_durability = 0, int32_t new_power = 0,
+            uint16_t new_fuel_use = 0):
+        Subsystem(new_name, new_tier, new_mass, new_durability, new_power),
+        fuel_use(new_fuel_use) { };
+
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
+};
+
+struct Thrusters : Subsystem {
+
+    uint16_t            speed;
+    uint16_t            maneuver;
+
+    Thrusters(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+            uint16_t new_durability = 0, int32_t new_power = 0,
+              uint16_t new_speed = 0, uint16_t new_maneuver = 0):
+        Subsystem(new_name, new_tier, new_mass, new_durability, new_power),
+        speed(new_speed),
+        maneuver(new_maneuver) { };
     
-    std::string         getName() const;
-    uint8_t             getTier() const;
-    uint16_t            getMass() const;
-    uint16_t            getDurability() const;
-    int32_t             getPower() const;
+    recsize_t           getSize() const;
 
-    void                setName(std::string name);
-    void                setTier(uint8_t tier);
-    void                setMass(uint16_t mass);
-    void                setDurability(uint16_t durability);
-    void                setPower(int32_t power);
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
 };
 
-class Reactor : Subsystem {
-private:
-    uint16_t            fuel_use_;
+struct FTLdrive : Subsystem {
 
-public:
-    Reactor(std::string name, uint8_t tier, uint16_t mass, 
-            uint16_t durability, uint32_t new_power, uint16_t new_fuel_use):
-        Subsystem(name, tier, mass, durability, new_power),
-        fuel_use_(new_fuel_use) { };
+    uint8_t             ftl_rating;
+
+    FTLdrive(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+            uint16_t new_durability = 0, int32_t new_power = 0,
+             uint8_t new_ftl_rating = 0):
+        Subsystem(new_name, new_tier, new_mass, new_durability, new_power),
+        ftl_rating(new_ftl_rating) { };
     
+    recsize_t           getSize() const;
 
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
 };
 
-class Thrusters : Subsystem {
-    uint16_t            speed_;
-    uint16_t            maneuver_;
+struct Computer : Subsystem {
+
+    uint16_t            processing_power;
+    uint16_t            memory;
+
+    Computer(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+            uint16_t new_durability = 0, int32_t new_power = 0,
+             uint16_t new_processing_power = 0, uint16_t new_memory = 0):
+        Subsystem(new_name, new_tier, new_mass, new_durability, new_power),
+        processing_power(new_processing_power),
+        memory(new_memory) { };
+
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
 };
 
-class FTLdrive : Subsystem {
-    uint8_t             ftl_rating_;
+struct Armor : Subsystem {
+
+    uint8_t             armor_rating;
+    uint8_t             hardness;
+
+    Armor(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+            uint16_t new_durability = 0, int32_t new_power = 0,
+          uint8_t new_armor_rating = 0, uint8_t new_hardness = 0):
+        Subsystem(new_name, new_tier, new_mass, new_durability, new_power),
+        armor_rating(new_armor_rating),
+        hardness(new_hardness) { };
+
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
 };
 
-class Computer : Subsystem {
-    uint16_t            processing_power_;
-    uint16_t            memory_;
-};
+struct Shields : Subsystem {
 
-class Armor : Subsystem {
-    uint8_t             armor_rating_;
-    uint8_t             hardness_;
-};
+    uint8_t             damage_reduction;
+    uint16_t            max_strength;
+    uint16_t            recharge_rate;
 
-class Shields : Subsystem {
-    uint8_t             damage_reduction_;
-    uint16_t            max_strength_;
-    uint16_t            recharge_rate_;
+    Shields(std::string new_name = "NONAME", uint8_t new_tier = 0, uint16_t new_mass = 0,
+            uint16_t new_durability = 0, int32_t new_power = 0,
+            uint8_t new_damage_reduction = 0, uint16_t new_max_strength = 0, uint16_t new_recharge_rate = 0):
+        Subsystem(new_name, new_tier, new_mass, new_durability, new_power),
+        damage_reduction(new_damage_reduction),
+        max_strength(new_max_strength),
+        recharge_rate(new_recharge_rate) { };
+
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
 };
 
 
