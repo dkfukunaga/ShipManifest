d984ec887288f2450ce77dac48a85afd6425651b
dkfukunaga <dkfukunaga@gmail.com>
2024-10-03 11:12:29 -0700

    implemented all serialize and deserialize functions

--- src/Subsystem.cpp
+++ src/Subsystem.cpp
@@ -51,12 +51,12 @@ recsize_t   Reactor::getSize() const {
 
 void        Reactor::serializeData(DataFile &file) const {
     Subsystem::serializeData(file);
-
+    file.write(&fuel_use);
 }
 
 void        Reactor::deserializeData(DataFile &file) {
     Subsystem::deserializeData(file);
-
+    file.read(&fuel_use);
 }
 
 
@@ -67,12 +67,14 @@ recsize_t   Thrusters::getSize() const {
 
 void        Thrusters::serializeData(DataFile &file) const {
     Subsystem::serializeData(file);
-
+    file.write(&speed);
+    file.write(&maneuver);
 }
 
 void        Thrusters::deserializeData(DataFile &file) {
     Subsystem::deserializeData(file);
-
+    file.read(&speed);
+    file.read(&maneuver);
 }
 
 
@@ -83,12 +85,12 @@ recsize_t   FTLdrive::getSize() const {
 
 void        FTLdrive::serializeData(DataFile &file) const {
     Subsystem::serializeData(file);
-
+    file.write(&ftl_rating);
 }
 
 void        FTLdrive::deserializeData(DataFile &file) {
     Subsystem::deserializeData(file);
-
+    file.read(&ftl_rating);
 }
 
 
@@ -100,12 +102,14 @@ recsize_t   Computer::getSize() const {
 
 void        Computer::serializeData(DataFile &file) const {
     Subsystem::serializeData(file);
-
+    file.write(&processing_power);
+    file.write(&memory);
 }
 
 void        Computer::deserializeData(DataFile &file) {
     Subsystem::deserializeData(file);
-
+    file.read(&processing_power);
+    file.read(&memory);
 }
 
 
@@ -116,12 +120,14 @@ recsize_t   Armor::getSize() const {
 
 void        Armor::serializeData(DataFile &file) const {
     Subsystem::serializeData(file);
-
+    file.write(&armor_rating);
+    file.write(&hardness);
 }
 
 void        Armor::deserializeData(DataFile &file) {
     Subsystem::deserializeData(file);
-
+    file.read(&armor_rating);
+    file.read(&hardness);
 }
 
 
@@ -132,12 +138,16 @@ recsize_t   Shields::getSize() const {
 
 void        Shields::serializeData(DataFile &file) const {
     Subsystem::serializeData(file);
-
+    file.write(&damage_reduction);
+    file.write(&max_strength);
+    file.write(&recharge_rate);
 }
 
 void        Shields::deserializeData(DataFile &file) {
     Subsystem::deserializeData(file);
-
+    file.read(&damage_reduction);
+    file.read(&max_strength);
+    file.read(&recharge_rate);
 }
 
 
