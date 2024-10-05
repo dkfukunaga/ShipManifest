f8d13fb557e8369937ff434455396fd082f0b480
dkfukunaga <dkfukunaga@gmail.com>
2024-10-04 21:17:00 -0700

    created weapon structs

---
+++ src/Weapon.cpp
@@ -0,0 +1,80 @@
+#include "Weapon.h"
+
+
+recsize_t   Weapon::getSize() const {
+    return Subsystem::getSize() + sizeof(size_class) + sizeof(damage_type) + sizeof(damage) + sizeof(range);
+}
+
+void        Weapon::serializeData(DataFile &file) const {
+    Subsystem::serializeData(file);
+    file.write(&size_class);
+    file.write(&damage_type);
+    file.write(&damage);
+    file.write(&range);
+}
+
+void        Weapon::deserializeData(DataFile &file) {
+    Subsystem::deserializeData(file);
+    file.read(&size_class);
+    file.read(&damage_type);
+    file.read(&damage);
+    file.read(&range);
+}
+
+
+
+
+
+recsize_t   Kinetic::getSize() const {
+    return Weapon::getSize() + sizeof(velocity) + sizeof(ammo_capacity);
+}
+
+void        Kinetic::serializeData(DataFile &file) const {
+    Weapon::serializeData(file);
+    file.write(&velocity);
+    file.write(&ammo_capacity);
+}
+
+void        Kinetic::deserializeData(DataFile &file) {
+    Weapon::deserializeData(file);
+    file.read(&velocity);
+    file.read(&ammo_capacity);
+}
+
+
+recsize_t   Beam::getSize() const {
+    return Weapon::getSize() + sizeof(shield_penetration) + sizeof(range_penalty) + sizeof(capacitor_charge);
+}
+
+void        Beam::serializeData(DataFile &file) const {
+    Weapon::serializeData(file);
+    file.write(&shield_penetration);
+    file.write(&range_penalty);
+    file.write(&capacitor_charge);
+}
+
+void        Beam::deserializeData(DataFile &file) {
+    Weapon::deserializeData(file);
+    file.read(&shield_penetration);
+    file.read(&range_penalty);
+    file.read(&capacitor_charge);
+}
+
+
+recsize_t   Missile::getSize() const {
+    return Weapon::getSize() + sizeof(velocity) + sizeof(evasion) + sizeof(tracking);
+}
+
+void        Missile::serializeData(DataFile &file) const {
+    Weapon::serializeData(file);
+    file.write(&velocity);
+    file.write(&evasion);
+    file.write(&tracking);
+}
+
+void        Missile::deserializeData(DataFile &file) {
+    Weapon::deserializeData(file);
+    file.read(&velocity);
+    file.read(&evasion);
+    file.read(&tracking);
+}
---
+++ src/Weapon.h
@@ -0,0 +1,106 @@
+
+#ifndef SHIP_WEAPON_H
+#define SHIP_WEAPON_H
+
+#include "DataFile.h"
+#include "Headers.h"
+#include "Serializable.h"
+#include "Subsystem.h"
+#include <string>
+#include <cstdint>
+
+enum class WeaponSizeClass : uint8_t {
+    light = 0,
+    medium,
+    heavy,
+};
+
+enum class DamageType :uint8_t {
+    kinetic,
+    energy,
+    explosive,
+};
+
+struct Weapon : Subsystem {
+    WeaponSizeClass     size_class      = WeaponSizeClass::light;
+    DamageType          damage_type     = DamageType::kinetic;
+    int32_t             damage          = 0;
+    int32_t             range           = 0;
+
+    Weapon() { };
+    Weapon(std::string new_name, uint8_t new_tier, uint16_t new_mass, uint16_t new_dur, int32_t new_power,
+            WeaponSizeClass new_size, DamageType new_dmg_type, int32_t new_damage, int32_t new_range):
+        Subsystem(new_name, new_tier, new_mass, new_dur, new_power),
+        size_class(new_size),
+        damage_type(new_dmg_type),
+        damage(new_damage),
+        range(new_range) { };
+    
+    recsize_t           getSize() const;
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
+};
+
+struct Kinetic : Weapon {
+    uint32_t            velocity        = 0;
+    uint32_t            ammo_capacity   = 0;
+
+    Kinetic() { };
+    Kinetic(std::string new_name, uint8_t new_tier, uint16_t new_mass, uint16_t new_dur, int32_t new_power,
+            WeaponSizeClass new_size, DamageType new_dmg_type, int32_t new_damage, int32_t new_range,
+            uint32_t new_vel, uint32_t new_ammo_cap):
+        Weapon(new_name, new_tier, new_mass, new_dur, new_power, new_size, new_dmg_type, new_damage, new_range),
+        velocity(new_vel),
+        ammo_capacity(new_ammo_cap) { };
+    
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
+};
+
+struct Beam : Weapon {
+    uint8_t             shield_penetration  = 0;
+    uint8_t             range_penalty       = 0;
+    uint16_t            capacitor_charge    = 0;
+
+    Beam() { };
+    Beam(std::string new_name, uint8_t new_tier, uint16_t new_mass, uint16_t new_dur, int32_t new_power,
+            WeaponSizeClass new_size, DamageType new_dmg_type, int32_t new_damage, int32_t new_range,
+            uint8_t new_shield_penetration, uint8_t new_range_pen, uint16_t new_cap_charge):
+        Weapon(new_name, new_tier, new_mass, new_dur, new_power, new_size, new_dmg_type, new_damage, new_range),
+        shield_penetration(new_shield_penetration),
+        range_penalty(new_range_pen),
+        capacitor_charge(new_cap_charge) { };
+    
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
+};
+
+struct Missile : Weapon {
+    uint32_t            velocity        = 0;
+    uint16_t            evasion         = 0;
+    uint16_t            tracking        = 0;
+
+    Missile() { };
+    Missile(std::string new_name, uint8_t new_tier, uint16_t new_mass, uint16_t new_dur, int32_t new_power,
+            WeaponSizeClass new_size, DamageType new_dmg_type, int32_t new_damage, int32_t new_range,
+            uint32_t new_vel, uint16_t new_evation, uint16_t new_tracking):
+        Weapon(new_name, new_tier, new_mass, new_dur, new_power, new_size, new_dmg_type, new_damage, new_range),
+        velocity(new_vel),
+        evasion(new_evation),
+        tracking(new_tracking) { };
+    
+    recsize_t           getSize() const;
+
+protected:
+    void                serializeData(DataFile &file) const;
+    void                deserializeData(DataFile &file);
+};
+
+#endif
