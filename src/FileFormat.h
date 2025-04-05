b2b584e0f9bbb1c0f941b4a831880f72d426f60b
dkfukunaga <dkfukunaga@gmail.com>
2025-04-05 01:14:16 -0700

    began implementing simplified file format

--- src/FileFormat.h
+++ src/FileFormat.h
@@ -2,8 +2,47 @@
 #ifndef FILE_FORMAT_H
 #define FILE_FORMAT_H
 
+#include <cstdint>
 
+enum class FileType : uint16_t {
+    index = 1,
+    data = 2,
+};
 
+enum class RecordType : uint16_t {
+    ship = 1,
+    ship_class = 2,
+    module = 3,         // not yet implemented
+    weapon = 4,
+    officer = 10,
+};
+
+#pragma pack(push, 1)
+// File Header
+struct FileHeader {
+    uint64_t        magic_number;       // Magic number to identify the file type (e.g., 0xCA7CAFE)
+    FileType        file_type;          // Enum for file type (Data, Index)
+    RecordType      record_type;        // Enum for record type
+    uint16_t        flags;              // Storage format flags (e.g., compressed, encrypted)
+    uint16_t        version;            // file version
+    uint32_t        data_offset;        // Byte offset of first data record
+    uint32_t        free_list_head;     // Byte offset of first free space
+    uint32_t        free_list_size;     // Total size of free space
+    uint32_t        fileSize;           // Total file size
+    uint64_t        created;            // Date created in epoch time
+    uint64_t        last_modified;      // Date last modified in epoch time
+    uint64_t        reserved[2];        // Reserved space for future expansion
+};
+
+// Record Header
+struct RecordHeader {
+    uint16_t        flags;              // active, deleted
+    uint16_t        padding;            // padding to ensure proper alignment
+    uint32_t        size;               // record size
+    uint32_t        next_free;          // offset of next free record
+    uint32_t        reserved;           // Reserved space for future expansion
+};
+#pragma pack(pop)
 
 
 
--- src/Serializable.h
+++ src/Serializable.h
@@ -1,70 +1,21 @@
 
-
-#ifndef SERIALIZABLE_DATA_H
-#define SERIALIZABLE_DATA_H
+#pragma once
+#ifndef SERIALIZABLE_H
+#define SERIALIZABLE_H
 
 #include "..\DataFile\src\DataFile.h"
-#include "Headers.h"
+#include "FileFormat.h"
 #include <cstdint>
 
 class Serializable {
 public:
     virtual             ~Serializable() = default;
-    virtual recsize_t   getSize() const = 0;
+    virtual uint32_t    getSize() const = 0;
 
-    offset_t            serialize(DataFile &file, index_t index, offset_t redirect) const;
-    offset_t            serialize(DataFile &file, index_t index, offset_t redirect, int64_t pos) const;
-    index_t             deserialize(DataFile &file);
-    index_t             deserialize(DataFile &file, int64_t pos);
-protected:
-    virtual void        writeHeader(DataFile &file, index_t index, recsize_t size, offset_t redirect) const = 0;
-    virtual void        serializeData(DataFile &file) const = 0;
-    virtual index_t     readHeader(DataFile &file) = 0;
-    virtual void        deserializeData(DataFile &file) = 0;
+    virtual uint32_t    serialize(DataFile &file) const;
+    virtual bool        deserialize(DataFile &file);
 };
 
-
-inline offset_t Serializable::serialize(DataFile &file, index_t index, offset_t redirect) const {
-    if (!file.isOpen()) {
-        return 0;
-    }
-
-    offset_t record_offset = file.getWritePos();
-
-    writeHeader(file, index, getSize(), redirect);
-    serializeData(file);
-    
-    return record_offset;
-}
-
-inline offset_t Serializable::serialize(DataFile &file, index_t index, offset_t redirect, int64_t pos) const {
-    if (!file.isOpen()) {
-        return 0;
-    }
-    file.setWritePos(pos);
-
-    return serialize(file, index, redirect);
-}
-
-inline index_t Serializable::deserialize(DataFile &file) {
-    if (!file.isOpen()) {
-        return 0;
-    }
-
-    index_t index = readHeader(file);
-    deserializeData(file);
-
-    return index;
-}
-
-inline index_t Serializable::deserialize(DataFile &file, int64_t pos) {
-    if (!file.isOpen()) {
-        return 0;
-    }
-    file.setReadPos(pos);
-
-    return deserialize(file);
-}
 
 
 #endif
