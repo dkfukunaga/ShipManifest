51d9f963d60746dad3a3883f99133ad698b47605
dkfukunaga <dkfukunaga@gmail.com>
2025-04-06 18:42:22 -0700

    created Weapon base class

--- src/Weapon.h
+++ src/Weapon.h
@@ -1,6 +1,61 @@
+#pragma once
+#ifndef WEAPON_H
+#define WEAPON_H
 
-// #ifndef SHIP_WEAPON_H
-// #define SHIP_WEAPON_H
+#include <string>
+#include <cstdint>
+#include "DataFile/src/DataFile.h"
+
+
+enum class WeaponType {
+    laser,
+    kinetic,
+    missile,
+};
+
+enum class WeaponSize {
+    light,
+    medium,
+    heavy,
+};
+
+
+
+class Weapon {
+public:
+    Weapon(uint32_t uid, WeaponType type, WeaponSize size, uint32_t damage, uint32_t range):
+        uid_(uid),
+        type_(type),
+        size_(size),
+        damage_(damage),
+        range_(range) { };
+
+    uint32_t                getUID() const;
+    uint32_t                getDamage() const;
+    uint32_t                getRange() const;
+
+    void                    setDamage(uint32_t damage);
+    void                    setRange(uint32_t range);
+
+    virtual                 ~Weapon() = default;
+    virtual uint32_t        getSize() const = 0;
+
+    virtual void            serialize(DataFile &file) const = 0;
+    virtual bool            deserialize(DataFile &file) = 0;
+private:
+    uint32_t                uid_;
+    WeaponType              type_;
+    WeaponSize              size_;
+
+    uint32_t                damage_;
+    uint32_t                range_;
+};
+
+
+
+
+#endif
+
 
 // #include "..\DataFile\src\DataFile.h"
 // #include "Headers.h"
@@ -115,5 +170,3 @@
 //     void                serializeData(DataFile &file) const;
 //     void                deserializeData(DataFile &file);
 // };
-
-// #endif
