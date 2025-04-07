db92d06cf28a305b3884133bdfd3d18ea5d7ecf9
dkfukunaga <dkfukunaga@gmail.com>
2025-04-06 18:41:49 -0700

    added include for DataFile

--- src/FileManager.h
+++ src/FileManager.h
@@ -5,25 +5,25 @@
 #include <cstdint>
 #include <string>
 #include <memory>
+#include "DataFile/src/DataFile.h"
 
-class DataFile;
 
 class FileManager {
 public:
 private:
-    uint32_t                    last_uid;
-    uint16_t                    version;
+    uint32_t                    last_uid_;
+    uint16_t                    version_;
 
-    std::unique_ptr<DataFile>   weapons_file;
-    std::unique_ptr<DataFile>   officers_file;
-    std::unique_ptr<DataFile>   ship_classes_file;
-    std::unique_ptr<DataFile>   ships_file;
+    std::unique_ptr<DataFile>   weapons_file_;
+    std::unique_ptr<DataFile>   officers_file_;
+    std::unique_ptr<DataFile>   ship_classes_file_;
+    std::unique_ptr<DataFile>   ships_file_;
 
-    std::string                 meta_data_file_name;
-    std::string                 weapons_file_name;
-    std::string                 officers_file_name;
-    std::string                 ship_classes_file_name;
-    std::string                 ships_file_name;
+    std::string                 meta_data_file_name_;
+    std::string                 weapons_file_name_;
+    std::string                 officers_file_name_;
+    std::string                 ship_classes_file_name_;
+    std::string                 ships_file_name_;
 
 
     const std::string           INI_EXTENSION = "ini";
