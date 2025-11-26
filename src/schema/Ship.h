c6cc3ce6f1a8927c9ae1eb1a4846afef1fc400cb
dkfukunaga <dkfukunaga@gmail.com>
2025-11-26 15:15:03 -0800

    added ship_crew field to Ship

--- src/schema/Ship.h
+++ src/schema/Ship.h
@@ -10,6 +10,7 @@ struct Ship {
     index_t         ship_id;
     std::string     ship_name;
     index_t         ship_class_id;
+    uint32_t        ship_crew;
 
     size_t          serialize(void* buf) const;
     size_t          deserialize(const void* buf);
